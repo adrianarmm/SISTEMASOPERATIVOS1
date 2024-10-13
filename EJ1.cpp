8dc1f100ddd85c44144788331ef7e8d1899981da
adrianarmm <areyemor@myuax.com>
2024-10-13 21:35:26 +0200

    CAMBIOS

--- EJ1.cpp
+++ EJ1.cpp
@@ -1,39 +1,36 @@
-#include <stdio.h>
-#include "utils.h"
 
-// Implementación de initArray
+#include "utils.h"
+#include <stdio.h>
+
 int initArray(arrayLength_t *arr) {
     if (arr == NULL) {
-        return -1;  // Si el puntero es NULL, retornamos -1
+        return -1; // Si el puntero es NULL, retorna -1
     }
 
-    arr->arrSize = 0;  // Inicializamos el tamaño en 0
-    arr->arrAdd = 0;   // Inicializamos la suma en 0
-
+    arr->arrSize = 0;
+    arr->arrAdd = 0;
     for (int i = 0; i < 10; i++) {
-        arr->arrInt[i] = -1;  // Inicializamos todas las posiciones del array a -1
+        arr->arrInt[i] = -1; // Inicializamos todas las posiciones con -1
     }
 
-    return 0;  // Retornamos 0 si todo fue bien
+    return 0; // Retorna 0 si todo está bien
 }
 
-// Implementación de addValue
 void addValue(arrayLength_t *arr, int value) {
-    if (arr->arrSize < 10) {  // Comprobamos si hay espacio en el array
-        arr->arrInt[arr->arrSize] = value;  // Añadimos el valor
-        arr->arrAdd += value;   // Actualizamos la suma
-        arr->arrSize++;         // Incrementamos el tamaño del array
+    if (arr->arrSize < 10) {
+        arr->arrInt[arr->arrSize] = value;
+        arr->arrAdd += value;
+        arr->arrSize++;
     } else {
         printf("Array lleno, no se puede agregar más valores.\n");
     }
 }
 
-// Implementación de printArray
 void printArray(arrayLength_t *arr) {
     printf("Elementos en el array: ");
-    for (int i = 0; i < arr->arrSize; i++) {  // Recorremos el array hasta el número de elementos almacenados
+    for (int i = 0; i < arr->arrSize; i++) {
         printf("%d ", arr->arrInt[i]);
     }
-    printf("\nNúmero de elementos: %d\n", arr->arrSize);  // Imprimimos el tamaño actual del array
-    printf("Suma de los elementos: %d\n", arr->arrAdd);   // Imprimimos la suma de los elementos
+    printf("\nNúmero de elementos: %d\n", arr->arrSize);
+    printf("Suma de los elementos: %d\n", arr->arrAdd);
 }
--- utils.h
+++ utils.h
@@ -2,16 +2,17 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-// Definición de la estructura con typedef para convertirla en un tipo de datos
+// Definición de la estructura
 typedef struct {
-    int arrInt[10];  // Array de enteros positivos con capacidad para 10 valores
-    int arrSize;     // Número de elementos almacenados en el array
-    int arrAdd;      // Suma de los elementos contenidos en el array
+    int arrInt[10];
+    int arrSize;
+    int arrAdd;
 } arrayLength_t;
 
-// Declaraciones de las funciones
-int initArray(arrayLength_t *arr);  // Inicializa el array
-void addValue(arrayLength_t *arr, int value);  // Agrega un valor al array
-void printArray(arrayLength_t *arr);  // Imprime el contenido del array
+// Declaración de las funciones
+int initArray(arrayLength_t *arr);
+void addValue(arrayLength_t *arr, int value);
+void printArray(arrayLength_t *arr);
 
 #endif
+
