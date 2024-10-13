b72220c20a6c318c5a4f299ccf3ba32b2c57613a
adrianarmm <areyemor@myuax.com>
2024-10-13 21:39:41 +0200

    REINICIO

--- EJ2.cpp
+++ EJ2.cpp
@@ -1,22 +1,4 @@
-#include "utils.h"
-#include <stdio.h>
 
-
-// Definición de la estructura con typedef para convertirla en un tipo de datos
-typedef struct {
-    int arrInt[10];  // Array de enteros positivos con capacidad para 10 valores
-    int arrSize;     // Número de elementos almacenados en el array
-    int arrAdd;      // Suma de los elementos contenidos en el array
-} arrayLength_t;
-
-// Función para inicializar el array con -1 y poner arrSize y arrAdd a 0
-int initArray(arrayLength_t *arr) {
-    if (arr == NULL) {
-        return -1; // Retornamos -1 en caso de que el puntero sea NULL
-    }
-
-    arr->arrSize = 0;  // Inicializamos el tamaño en 0
-    arr->arrAdd = 0;   // Inicializamos la suma en 0
 
     for (int i = 0; i < 10; i++) {
         arr->arrInt[i] = -1;  // Inicializamos todas las posiciones del array a -1
