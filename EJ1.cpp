89836a3af99a05acc98ac12766ecac4848861ffd
adrianarmm <areyemor@myuax.com>
2024-10-13 20:51:18 +0200

    Implementar función initArray para inicializar la estructura

--- EJ1.cpp
+++ EJ1.cpp
@@ -6,3 +6,12 @@ typedef struct {
     int arrSize;     // Número de elementos almacenados en el array
     int arrAdd;      // Suma de los elementos contenidos en el array
 } arrayLength_t;
+
+// Función para inicializar la estructura
+void initArray(arrayLength_t *arr) {
+    arr->arrSize = 0;  // Inicializamos el tamaño en 0
+    arr->arrAdd = 0;   // Inicializamos la suma en 0
+    for (int i = 0; i < 10; i++) {
+        arr->arrInt[i] = 0;  // Inicializamos todos los elementos del array en 0
+    }
+}
